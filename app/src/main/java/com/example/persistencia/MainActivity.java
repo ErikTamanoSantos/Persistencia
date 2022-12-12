b82c4a0fa9fa15f7fc2e1d45c14e0c14e9a5d641
Erik Tamano <eriktamano@gmail.com>
2022-12-12 15:46:31 +0100

    Arreglat error bucle infinit, Layout cambiat

--- app/src/main/java/com/example/persistencia/MainActivity.java
+++ app/src/main/java/com/example/persistencia/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -19,23 +21,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.saveButton);
-
-        try {
-            OutputStream os = openFileOutput("dades.txt", MODE_PRIVATE);
-            String message= "Hello, World!";
-            os.write(message.getBytes());
-            os.close();
-            readFile();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Button button = findViewById(R.id.saveFile);
+        EditText fileContent = findViewById(R.id.fileContent);
+        readFile();
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 try {
                     OutputStream os = openFileOutput("dades.txt", MODE_PRIVATE);
-                    TextView text = findViewById(R.id.inputText);
-                    String message = String.valueOf(text.getText());
+                    String message = String.valueOf(fileContent.getText());
                     os.write(message.getBytes());
                     os.close();
                     readFile();
@@ -50,14 +43,17 @@ public class MainActivity extends AppCompatActivity {
     public void readFile(){
         File path = getFilesDir();
         File file = new File(path,"dades.txt");
+        if (!file.exists()) {
+            createFile();
+        }
         StringBuilder stringBuilder = new StringBuilder();
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
             String line = br.readLine();
             while (line != null) {
-                br.readLine();
                 stringBuilder.append(line);
                 stringBuilder.append('\n');
+                line = br.readLine();
             }
             br.close();
         }
@@ -66,5 +62,16 @@ public class MainActivity extends AppCompatActivity {
         }
         TextView fileContent = (TextView)findViewById(R.id.fileContent);
         fileContent.setText(stringBuilder.toString());
+    }
+
+    public void createFile() {
+        try {
+            OutputStream os = openFileOutput("dades.txt", MODE_PRIVATE);
+            String message= "Hello, World!";
+            os.write(message.getBytes());
+            os.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
